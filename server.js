89d52bcf60df50810a76fd8c04a45265a8b9f5aa
shoddyshaman <dhananjoy.tiwari@gmail.com>
2021-10-12 10:01:01 -0600

    bootstrapped server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+const port = process.env.port || 4400
+
+
+app.listen(port, () => console.log(`server up and running on ${port}!`))
