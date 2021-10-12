e96ad4134db9c6a58afb86cc8e70067ba2892ce8
shoddyshaman <dhananjoy.tiwari@gmail.com>
2021-10-12 11:02:20 -0600

    added css code

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use('/style', express.static('./public/styles.css'))
+app.use('/style', express.static(path.join(__dirname,'./public/styles.css')))
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
